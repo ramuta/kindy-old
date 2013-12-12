17f2aa9bcc6f17afe92a52d1a9ef33b2027be44c
Matej Ramuta <matej.ramuta@gmail.com>
2013-12-12 16:38:55 +0100

    list of childcares at home page

--- kindy/views.py
+++ kindy/views.py
@@ -5,5 +5,9 @@ from childcare.models import Childcare
 
 @login_required
 def home(request):
-    childcare_list = Childcare.objects.filter(managers__id=request.user.pk)
-    return render(request, 'kindy/home.html', {'childcare_list': childcare_list})
+    childcare_list_manager = Childcare.objects.filter(managers__id=request.user.pk)
+    childcare_list_employee = Childcare.objects.filter(employees__id=request.user.pk)
+    childcare_list_parent = Childcare.objects.filter(parents__id=request.user.pk)
+    return render(request, 'kindy/home.html', {'childcare_list_manager': childcare_list_manager,
+                                               'childcare_list_employee': childcare_list_employee,
+                                               'childcare_list_parent': childcare_list_parent})
