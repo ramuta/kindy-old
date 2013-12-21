5107543579111e32c799e4dfd854d62eab4fcb52
Matej Ramuta <matej.ramuta@gmail.com>
2013-12-21 13:48:35 +0100

    setting up email

--- kindy/settings.py
+++ kindy/settings.py
@@ -224,8 +224,8 @@ else:  # TODO
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = 'yourgmailaccount@gmail.com'
-    EMAIL_HOST_PASSWORD = 'yourgmailpassword'
+    EMAIL_HOST_USER = 'info@getkindy.com'
+    EMAIL_HOST_PASSWORD = os.environ["EMAIL_PASSWORD"]
 
 # guardian
 ANONYMOUS_USER_ID = -1
--- utils/secret.py
+++ utils/secret.py
@@ -3,7 +3,12 @@ Everything that's secret. IMPORTANT: Add this file to .gitignore!
 '''
 
 SECRET_KEY = 'rlx@p)7o*z!sf=cm26aded5^h&5pgfglqq)%c+)mdk2os#$e%1'
+EMAIL_PASSWORD = 'k1ndy123'
 
 
 def get_secret_key():
     return SECRET_KEY
+
+
+def get_email_password():
+    return EMAIL_PASSWORD
