608b07830ef2f55cf2660b11cddf0e08dc2a4023
Matej Ramuta <matej.ramuta@gmail.com>
2014-01-02 15:41:38 +0100

    Home childcare list (all in one list, removed duplicates)

--- kindy/views.py
+++ kindy/views.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from childcare.models import Childcare
@@ -8,6 +9,6 @@ def home(request):
     childcare_list_manager = Childcare.objects.filter(managers__id=request.user.pk)
     childcare_list_employee = Childcare.objects.filter(employees__id=request.user.pk)
     childcare_list_parent = Childcare.objects.filter(parents__id=request.user.pk)
-    return render(request, 'kindy/home.html', {'childcare_list_manager': childcare_list_manager,
-                                               'childcare_list_employee': childcare_list_employee,
-                                               'childcare_list_parent': childcare_list_parent})
+
+    all_childcare_list = list(set(chain(childcare_list_manager, childcare_list_employee, childcare_list_parent)))  # set for removing duplicates, chain for chaining lists
+    return render(request, 'kindy/home.html', {'all_childcare_list': all_childcare_list})
