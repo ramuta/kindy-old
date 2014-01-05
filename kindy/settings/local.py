18fda6bc09eec8ea8e2ec09ce099e4b87c2e7057
Matej Ramuta <matej.ramuta@gmail.com>
2014-01-05 22:02:33 +0100

    Logging prod settings fix

--- kindy/settings/local.py
+++ kindy/settings/local.py
@@ -34,6 +34,35 @@ CKEDITOR_UPLOAD_PATH = MEDIA_ROOT + 'ckeditor/'
 # Nastavi env.variable na Heroku
 SECRET_KEY = get_secret_key()
 
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 # emailing
 # run in terminal: python -m smtpd -n -c DebuggingServer localhost:1025
 EMAIL_HOST = 'localhost'
