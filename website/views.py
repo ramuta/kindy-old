da82e59bff1b373b9441cccc5b5dcd2304657f22
Matej Ramuta <matej.ramuta@gmail.com>
2014-01-07 14:44:58 +0100

    Website index: added latest news and page with order 1

--- website/views.py
+++ website/views.py
@@ -12,12 +12,16 @@ log_prefix = '[kindylog]'
 
 def website(request, childcare_slug):
     childcare = get_object_or_404(Childcare, slug=childcare_slug)
-    website_news_list = News.objects.filter(childcare=childcare, public=True)
+    #website_news_list = News.objects.filter(childcare=childcare, public=True)
+    website_news_latest = News.objects.filter(childcare=childcare, public=True)[:1].get()
     pages_list = Page.objects.filter(childcare=childcare)
+    page_first = Page.objects.filter(childcare=childcare)[:1].get()
     log.info(log_prefix+'Website index (childcare: %s)' % childcare.name)
     return render(request, 'themes/'+childcare.theme.computer_name+'/index.html', {'childcare': childcare,
-                                                                                   'news_list': website_news_list,
-                                                                                   'pages_list': pages_list})
+                                                                                   #'news_list': website_news_list,
+                                                                                   'news_latest': website_news_latest,
+                                                                                   'pages_list': pages_list,
+                                                                                   'page': page_first})
 
 
 def website_news(request, childcare_slug):
